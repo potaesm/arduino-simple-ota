c7b869f419c5fe5db6c2296886585ee3b826bd7b
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-02-04 15:03:10 +0530

    Add log toggle

--- SimpleOta.cpp
+++ SimpleOta.cpp
@@ -15,6 +15,8 @@
 #error "No ESP8266 or ESP32 detected"
 #endif
 
+#define DEVMODE 1
+
 // Download file via proxy server
 // char *proxyServerURL = "http://node-pxy.herokuapp.com";
 
@@ -28,17 +30,23 @@ bool __downloadFileToSPIFFS(WiFiClient wiFiClient, String fileURL, String fileNa
     HTTPClient Http;
     // fileURL = String(proxyServerURL) + "/download-file?url=" + fileURL;
     Http.begin(wiFiClient, fileURL);
+#if defined(DEVMODE)
     Serial.printf("Begin HTTP GET request: %s\n", fileURL.c_str());
+#endif
     int httpCode = Http.GET();
     if (httpCode > 0)
     {
+#if defined(DEVMODE)
         Serial.printf("Status code: %d\n", httpCode);
+#endif
         if (httpCode == HTTP_CODE_OK)
         {
             file = SPIFFS.open(fileName, "w");
             int len = Http.getSize();
+#if defined(DEVMODE)
             Serial.printf("File size: %d bytes\n", len);
             Serial.print("Downloading...\n");
+#endif
             uint8_t buff[512] = {0};
             WiFiClient *stream = Http.getStreamPtr();
             while (Http.connected() && (len > 0 || len == -1))
@@ -49,8 +57,10 @@ bool __downloadFileToSPIFFS(WiFiClient wiFiClient, String fileURL, String fileNa
                 if (size)
                 {
                     int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+#if defined(DEVMODE)
                     // Serial.printf("Read: %d bytes\n", c);
                     // Serial.printf("Remaining: %d bytes\n", size);
+#endif
                     file.write(buff, c);
                     if (len > 0)
                         len -= c;
@@ -59,13 +69,17 @@ bool __downloadFileToSPIFFS(WiFiClient wiFiClient, String fileURL, String fileNa
                 ESP.wdtFeed();
             }
             isDownloaded = true;
+#if defined(DEVMODE)
             Serial.print("HTTP connection closed or file end\n");
+#endif
         }
     }
+#if defined(DEVMODE)
     else
     {
         Serial.printf("HTTP GET failed: %s\n", Http.errorToString(httpCode).c_str());
     }
+#endif
     Http.end();
     file.close();
     return isDownloaded;
@@ -73,29 +87,41 @@ bool __downloadFileToSPIFFS(WiFiClient wiFiClient, String fileURL, String fileNa
 
 void __updateFromSPIFFS(String fileName)
 {
+#if defined(DEVMODE)
     Serial.println("Opening update file...");
+#endif
     if (!SPIFFS.exists(fileName))
     {
+#if defined(DEVMODE)
         Serial.println("Update file not found");
+#endif
         return;
     }
     File file = SPIFFS.open(fileName, "r");
+#if defined(DEVMODE)
     Serial.println("Updating...");
+#endif
     size_t fileSize = file.size();
     if (!Update.begin(fileSize))
     {
+#if defined(DEVMODE)
         Update.printError(Serial);
+#endif
         return;
     }
     Update.writeStream(file);
     if (!Update.end())
     {
+#if defined(DEVMODE)
         Update.printError(Serial);
+#endif
         return;
     }
     file.close();
-    Serial.println("Update success!");
+#if defined(DEVMODE)
+    Serial.println("Updated!");
     Serial.println("Rebooting...");
+#endif
     ESP.restart();
 }
 
@@ -105,14 +131,22 @@ void initializeOta(WiFiClient wiFiClient, char *wifiSSID, char *wifiPassword, St
     __wifiPassword = wifiPassword;
     ESP.wdtDisable();
     ESP.wdtEnable(WDTO_8S);
+#if defined(DEVMODE)
     Serial.println("Connecting WiFi...");
+#endif
     connectWifi(wifiSSID, wifiPassword);
+#if defined(DEVMODE)
     Serial.println("Checking Internet...");
+#endif
     checkInternet(wiFiClient, wifiSSID, wifiPassword);
+#if defined(DEVMODE)
     Serial.println("Initializing SPIFFS...");
+#endif
     if (!SPIFFS.begin())
     {
+#if defined(DEVMODE)
         Serial.println("Unable to activate SPIFFS");
+#endif
         return;
     }
     if (SPIFFS.exists(fileName))
@@ -124,5 +158,7 @@ void initializeOta(WiFiClient wiFiClient, char *wifiSSID, char *wifiPassword, St
         __updateFromSPIFFS(fileName);
         return;
     }
+#if defined(DEVMODE)
     Serial.println("Update file is not downloaded");
+#endif
 }
